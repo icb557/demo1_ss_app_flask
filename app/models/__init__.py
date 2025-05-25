04a8cf9db22314ec3ec87ab6da08fb2529a48068
juansecod <juan.sebastianri28@gmail.com>
2025-05-25 14:10:14 -0500

    FAD-26 - Create TravelDiary model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,7 @@
 """Models package."""
 from app.models.user import User
 from app.models.task import Task
+from app.models.travel_diary import TravelDiary
+from app.models.activity import Activity
 
-__all__ = ['User', 'Task']
+__all__ = ['User', 'Task', 'TravelDiary', 'Activity']
