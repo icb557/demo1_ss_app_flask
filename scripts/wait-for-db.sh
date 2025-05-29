da7b38e1804c71a8dcd0d0c7904a71ba412c1705
demartinez771 <demartinez771@udenar.edu.co>
2025-05-29 15:18:27 -0500

    ok pipelines

--- scripts/wait-for-db.sh
+++ scripts/wait-for-db.sh
@@ -1,27 +1,16 @@
 #!/bin/sh
-# Simple wait-for-db script
+# wait-for-db.sh
+
+set -e
 
 host="$1"
 shift
 cmd="$@"
 
-echo "Waiting for $host:5432..."
-
-for i in $(seq 30); do
-    if nc -z "$host" 5432 > /dev/null 2>&1; then
-        echo "$host is ready!"
-        break
-    fi
-    
-    if [ $i -eq 30 ]; then
-        echo "Timeout waiting for $host"
-        exit 1
-    fi
-    
-    sleep 2
+until PGPASSWORD=$DB_PASSWORD psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
 
-# Execute command if provided
-if [ -n "$cmd" ]; then
-    exec $cmd
-fi
+>&2 echo "Postgres is up - executing command"
+exec $cmd 
