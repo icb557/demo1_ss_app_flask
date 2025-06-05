fdefd30798d1bb3448768d28e9e1a1b8664d0110
Juan Sebastian <juan.sebastianri28@gmail.com>
2025-06-05 13:42:28 -0500

    Fix: change database to testing in config.py

--- config.py
+++ config.py
@@ -27,6 +27,6 @@ class TestingConfig(Config):
 class IntegrationTestingConfig(Config):
     """Testing configuration for integration tests."""
     TESTING = True
-    DB_NAME = os.environ.get('TEST_DB_NAME', 'life_organizer_test')
+    DB_NAME = os.environ.get('TEST_DB_NAME', 'life_organizer')
     SQLALCHEMY_DATABASE_URI = f'postgresql://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{DB_NAME}'
     WTF_CSRF_ENABLED = False 
