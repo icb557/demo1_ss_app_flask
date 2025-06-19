1a8835100e8ff2284f26553de3603c3431ca63e7
Isac Cortés Buitrago <144753785+icb557@users.noreply.github.com>
2025-06-19 00:15:36 -0500

    Update config.py
    edit port type

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     DB_USER = os.environ.get('DB_USER', 'postgres')
     DB_PASSWORD = os.environ.get('DB_PASSWORD', 'postgres')
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
-    DB_PORT = os.environ.get('DB_PORT', '5432')
+    DB_PORT = int(os.getenv("DB_PORT", 5432))
     DB_NAME = os.environ.get('DB_NAME', 'life_organizer')
     
     # SQLAlchemy
